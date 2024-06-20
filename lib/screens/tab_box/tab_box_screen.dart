e499fff429a44cae118fdf4270125d99563b4548
abdushukur77 <asrolov01@mail.ru>
2024-06-20 23:29:53 +0500

    vacancy

--- lib/screens/tab_box/tab_box_screen.dart
+++ lib/screens/tab_box/tab_box_screen.dart
@@ -6,8 +6,6 @@ import 'package:e_worker/screens/tab_box/profile_screen/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 
 class TabBoxScreen extends StatefulWidget {
   const TabBoxScreen({super.key});
@@ -17,170 +15,58 @@ class TabBoxScreen extends StatefulWidget {
 }
 
 class _TabBoxScreenState extends State<TabBoxScreen> {
-  List<Widget> screens = [];
-  var currentIndex = 0;
+  int currentIndex = 0;
 
-  List<IconData> listOfIcons = [
-    Icons.home_rounded,
-    Icons.person_rounded,
+  List<Widget> screens = [
+    const HomeScreen(),
+    const ProfileScreen(),
   ];
-
-  List<String> listOfStrings = [
-    'Home',
-    'Profile',
-  ];
-
-  @override
-  void initState() {
-    screens = [
-      const HomeScreen(),
-      const ProfileScreen(),
-    ];
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
-
     return AnnotatedRegion(
       value: SystemUiOverlayStyle(
-
         statusBarColor: Colors.black,
         statusBarIconBrightness: Brightness.light,
         systemNavigationBarColor: Colors.black,
         systemNavigationBarIconBrightness: Brightness.dark,
         systemNavigationBarDividerColor: Colors.black,
-        statusBarBrightness: Brightness.dark
-
-
+        statusBarBrightness: Brightness.dark,
       ),
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Stack(
-          children: [
-            IndexedStack(
-              index: currentIndex,
-              children: screens,
+        body: IndexedStack(
+          index: currentIndex,
+          children: screens,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded),
+              label: 'Home',
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                // margin: EdgeInsets.symmetric(horizontal:width * 0.05),
-                height: width * .155,
-                decoration:const BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: ListView.builder(
-                  itemCount: 2,
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.symmetric(horizontal: width * .02),
-                  itemBuilder: (context, index) => InkWell(
-                    onTap: () {
-                      setState(() {
-                        currentIndex = index;
-                        HapticFeedback.lightImpact();
-                      });
-                    },
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Stack(
-                          children: [
-                            AnimatedContainer(
-                              duration: const Duration(seconds: 1),
-                              curve: Curves.fastLinearToSlowEaseIn,
-                              width: index == currentIndex ? width * .32 : width * .50,
-                              alignment: Alignment.center,
-                              child: AnimatedContainer(
-                                duration: const Duration(seconds: 1),
-                                curve: Curves.fastLinearToSlowEaseIn,
-                                height: index == currentIndex ? width * .12 : 0,
-                                width: index == currentIndex ? width * .50 : 0,
-                                decoration: BoxDecoration(
-                                  color: index == currentIndex
-                                      ? Colors.blueAccent.withOpacity(.2)
-                                      : Colors.transparent,
-                                  borderRadius: BorderRadius.circular(50),
-                                ),
-                              ),
-                            ),
-                            SizedBox(width: 100.w),
-
-                            AnimatedContainer(
-                              duration: const Duration(seconds: 1),
-                              curve: Curves.fastLinearToSlowEaseIn,
-                              width: index == currentIndex ? width * .31 : width * .50,
-                              alignment: Alignment.center,
-                              child: Stack(
-                                children: [
-                                  Row(
-                                    children: [
-                                      AnimatedContainer(
-                                        duration: const Duration(seconds: 1),
-                                        curve: Curves.fastLinearToSlowEaseIn,
-                                        width: index == currentIndex ? width * .13 : 0,
-                                      ),
-                                      AnimatedOpacity(
-                                        opacity: index == currentIndex ? 1 : 0,
-                                        duration: const Duration(seconds: 1),
-                                        curve: Curves.fastLinearToSlowEaseIn,
-                                        child: Text(
-                                          index == currentIndex ? listOfStrings[index] :'',
-                                          style: const TextStyle(
-                                            color: Colors.blueAccent,
-                                            fontWeight: FontWeight.w600,
-                                            fontSize: 15,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-
-                                  Row(
-                                    children: [
-                                      AnimatedContainer(
-                                        duration: const Duration(seconds: 1),
-                                        curve: Curves.fastLinearToSlowEaseIn,
-                                        width: index == currentIndex ? width * .03 : 20,
-                                      ),
-                                      Icon(
-                                        listOfIcons[index],
-                                        size: width * .076,
-                                        color: index == currentIndex
-                                            ? Colors.blueAccent
-                                            : Colors.black26,
-                                      ),
-
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_rounded),
+              label: 'Profile',
             ),
           ],
         ),
-
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.blue,
-          onPressed: (){
+          onPressed: () {
             context.read<VacancyBloc>().add(ChangeToInitialState());
-            Navigator.push(context,MaterialPageRoute(builder: (context){
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
               return const CreateScreen();
             }));
           },
-          child: const Icon(Icons.add,color:Colors.white,),
+          child: const Icon(Icons.add, color: Colors.white),
         ),
       ),
     );
